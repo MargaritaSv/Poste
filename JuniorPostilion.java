931e517ce8afedfb1ecae1dbdf63635e8c124d32
MargaritaSv <margarita.esti@gmail.com>
2016-07-26 22:27:12 +0300

    Create classes JuniorPostilion.java and Postilion.java with constructor

---
+++ JuniorPostilion.java
@@ -0,0 +1,11 @@
+package poste;
+
+/**
+ * Created by Magi on 26.7.2016 г..
+ */
+public class JuniorPostilion extends Person {
+
+    public JuniorPostilion(String name, String secondName) {
+        super(name, secondName);
+    }
+}
---
+++ Postilion.java
@@ -0,0 +1,12 @@
+package poste;
+
+/**
+ * Created by Magi on 26.7.2016 г..
+ */
+public class Postilion extends JuniorPostilion {
+
+    public Postilion(String name, String secondName) {
+        super(name, secondName);
+    }
+
+}
