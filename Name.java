5ffc9690c1ff6c6f3cdf732ead60ca9c47537d5b
MargaritaSv <margarita.esti@gmail.com>
2016-08-22 10:15:32 +0300

    class Name

---
+++ Name.java
@@ -0,0 +1,10 @@
+public class Name {
+	private String name;
+
+	public Name(String name) {
+		if (name == null || name.trim().length() == 0) {
+			throw new IllegalArgumentException();
+		}
+		this.name = name;
+	}
+}
