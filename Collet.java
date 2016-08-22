6b9fe1073909bfdc56876579e7b794a7cda8dab4
MargaritaSv <margarita.esti@gmail.com>
2016-08-22 15:28:48 +0300

    class Collet - variables and validates

--- Collet.java
+++ Collet.java
@@ -1,5 +1,34 @@
+public class Collet extends Letter {
 
+	private int width;
+	private int lenght;
+	private int hight;
+	private boolean isFragile;
 
-public class Collet {
+	public Collet(Citizens citizen1, Citizens citizens2, int width, int lenght,
+			int hight, boolean isFragile) {
+		super(citizen1, citizens2);
 
+		if (!checkValue(width)) {
+			throw new IllegalArgumentException("Width is not valis ->" + width);
+		}
+		this.width = width;
+
+		if (!checkValue(lenght)) {
+			throw new IllegalArgumentException("Length is not valis ->"
+					+ lenght);
+		}
+		this.lenght = lenght;
+
+		if (!checkValue(hight)) {
+			throw new IllegalArgumentException("Hight is not valis ->" + hight);
+		}
+		this.hight = hight;
+
+		this.isFragile = isFragile;
+	}
+
+	private static boolean checkValue(int value) {
+		return value <= 0;
+	}
 }
