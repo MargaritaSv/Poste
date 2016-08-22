56656866b10805aa21aba71cd0509283b09ed0cd
MargaritaSv <margarita.esti@gmail.com>
2016-08-22 14:14:59 +0300

    create class Letter

--- Letter.java
+++ Letter.java
@@ -1,7 +1,9 @@
-
-
 public class Letter {
 	private static float price = 0.5f;
-	
-	
+
+	private TwoPeople connection;
+
+	public Letter(Citizens citizen1, Citizens citizens2) {
+		this.connection = new TwoPeople(citizen1, citizens2);
+	}
 }
